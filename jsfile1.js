db1f6e12772a81d68282e1b442dbab378300fc45
Azmanur1 <azmanurrahman557@gmail.com>
2025-02-02 09:19:16 +0600

    initial html css layout

---
+++ jsfile1.js
@@ -0,0 +1,13 @@
+// document.getElementById("canvas").textContent = "Hello ";
+const canvas = document.querySelector("#canvas");
+// for(let i=0;i<16;i++){
+//    const div =  document.createElement("div");
+//    canvas.appendChild(div);
+// }
+
+for(let i=0;i<16;i++){
+    for(let j=0;j<16;j++){
+        const div =  document.createElement("div");
+        canvas.appendChild(div);
+    }
+ }
