c10579ba4d5bf2ea10d32f8f287f73f0cd6d5f05
Azmanur1 <azmanurrahman557@gmail.com>
2025-02-02 15:53:16 +0600

    Eraser function incomplete and randomized Color incomplet

--- jsfile1.js
+++ jsfile1.js
@@ -1,13 +1,46 @@
-// document.getElementById("canvas").textContent = "Hello ";
-const canvas = document.querySelector("#canvas");
-// for(let i=0;i<16;i++){
-//    const div =  document.createElement("div");
-//    canvas.appendChild(div);
-// }
 
-for(let i=0;i<16;i++){
-    for(let j=0;j<16;j++){
-        const div =  document.createElement("div");
-        canvas.appendChild(div);
+
+
+
+function generateGrid()
+{
+    const gridSizeInput = document.getElementById("gridSizeInput");
+    let size = gridSizeInput.value || gridSizeInput.defaultValue;
+    size = parseInt(size);
+    if(isNaN(size) || size<2 || size>100){
+        alert("Please Enter number between 2 and 100");
+        return;
     }
- }
+
+    const container = document.getElementById("gridContainer");
+    container.innerHTML = '';
+    const squareSize = 960/size+'px';
+    console.log(squareSize);
+    for(let i=0;i<size*size;i++){
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.width = squareSize;
+        square.style.height = squareSize;
+        
+        square.addEventListener("mouseover", () => {
+            if(square.classList.contains(".hovered")){
+                square.classList.remove("hovered")
+            }
+            else{
+                square.classList.add("hovered");
+            }
+            
+            
+        });
+
+        container.appendChild(square);
+        
+    }
+}
+
+function reset() 
+{
+    document.querySelectorAll(".hovered").forEach(item => {
+        item.classList.remove("hovered");
+    });
+}
